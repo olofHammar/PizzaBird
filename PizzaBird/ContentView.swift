3e4b5662b1b0ee3028f169171b5fb6f7b6dc42f2
olofHammar <70969820+olofHammar@users.noreply.github.com>
2021-08-09 13:32:29 +0200

    Initial Commit

---
+++ PizzaBird/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PizzaBird
+//
+//  Created by Olof Hammar on 2021-08-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PizzaBird/PizzaBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaBirdApp.swift
+//  PizzaBird
+//
+//  Created by Olof Hammar on 2021-08-09.
+//
+
+import SwiftUI
+
+@main
+struct PizzaBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
